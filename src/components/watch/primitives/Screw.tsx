695c8958c08fc40c31eb7dd0f37d037cab74d9e9
zgbrenner <zgbrenner@gmail.com>
2026-07-02 19:06:11 -0700

    Orient screw primitives in watch plane

--- src/components/watch/primitives/Screw.tsx
+++ src/components/watch/primitives/Screw.tsx
@@ -7,13 +7,13 @@ type ScrewProps = {
 export function Screw({ radius = 0.012, length = 0.02, color = "#9ba5ad" }: ScrewProps) {
   return (
     <group>
-      <mesh>
-        <cylinderGeometry args={[radius, radius, length, 12]} />
-        <meshStandardMaterial color={color} metalness={0.8} roughness={0.3} />
+      <mesh rotation={[Math.PI / 2, 0, 0]}>
+        <cylinderGeometry args={[radius, radius, length, 20]} />
+        <meshStandardMaterial color={color} metalness={0.86} roughness={0.24} />
       </mesh>
-      <mesh position={[0, length / 2, 0]}>
-        <boxGeometry args={[radius * 1.6, radius * 0.3, radius * 0.3]} />
-        <meshStandardMaterial color={color} metalness={0.8} roughness={0.3} />
+      <mesh position={[0, 0, length / 2 + 0.001]}>
+        <boxGeometry args={[radius * 1.7, radius * 0.26, radius * 0.14]} />
+        <meshStandardMaterial color="#59616b" metalness={0.72} roughness={0.34} />
       </mesh>
     </group>
   )
