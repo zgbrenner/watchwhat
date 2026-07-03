c7abc36937fff17f993aa1972c2c4bd994f1b41d
zgbrenner <zgbrenner@gmail.com>
2026-07-02 19:06:31 -0700

    Orient strap primitives around case plane

--- src/components/watch/primitives/Strap.tsx
+++ src/components/watch/primitives/Strap.tsx
@@ -7,9 +7,21 @@ type StrapProps = {
 
 export function Strap({ width = 0.18, length = 0.9, thickness = 0.015, color = "#22262b" }: StrapProps) {
   return (
-    <mesh>
-      <boxGeometry args={[width, thickness, length]} />
-      <meshStandardMaterial color={color} metalness={0.1} roughness={0.8} />
-    </mesh>
+    <group>
+      <mesh position={[0, 0.52, 0]}>
+        <boxGeometry args={[width, length * 0.55, thickness]} />
+        <meshStandardMaterial color={color} metalness={0.08} roughness={0.82} />
+      </mesh>
+      <mesh position={[0, -0.52, 0]}>
+        <boxGeometry args={[width, length * 0.55, thickness]} />
+        <meshStandardMaterial color={color} metalness={0.08} roughness={0.82} />
+      </mesh>
+      {[-0.05, 0, 0.05].map((x) => (
+        <mesh key={x} position={[x, -0.62, thickness / 2 + 0.001]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.01, 0.01, 0.002, 16]} />
+          <meshStandardMaterial color="#0f1115" roughness={0.9} />
+        </mesh>
+      ))}
+    </group>
   )
 }
